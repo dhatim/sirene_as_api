8ad92d441bb8ca114d3f0202030a55303fd1438f
Samson Zhang <smnzhang@hotmail.com>
2019-07-28 00:34:24 +0200

    revert v1/siret to exact siret lookup, add regex control

--- app/controllers/api/v1/siret_controller.rb
+++ app/controllers/api/v1/siret_controller.rb
@@ -1,9 +1,8 @@
 class API::V1::SiretController < ApplicationController
   def show
     query = siret_params[:siret]
-    if /\A\d{9,14}\z/.match?(query)
-      r = Etablissement.where("siret LIKE :query",
-                              query: "#{query}%")
+    if /\A\d{14}\z/.match?(query)
+      r = Etablissement.find_by(siret: query)
       if r.nil?
         render json: { message: 'no results found' }, status: 404
       else

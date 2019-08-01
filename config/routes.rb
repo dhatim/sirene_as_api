0bbb016920270557f591a9f1b566244e6320fc9a
Samson Zhang <szhang@dhatim.com>
2019-08-01 11:10:03 +0200

    hotfix dot in search path conflict

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,17 @@
 Rails.application.routes.draw do
 
   concern :v1_routes do
-    get 'siret/:siret' => '/api/v1/siret#show'
-    get 'siren/:siren' => '/api/v1/siren#show'
-    get 'siren_like/:siren' => '/api/v1/siren_like#show'
-    get 'siret_like/:siret' => '/api/v1/siret_like#show'
-    get 'nom_adresse/:text' => '/api/v1/nom_adresse#show'
-    get 'full_text/:text' => '/api/v1/full_text#show'
-    get 'suggest/:suggest_query' => '/api/v1/suggest#show'
-    get 'near_etablissement/:siret' => '/api/v1/near_etablissement#show'
-    get 'near_etablissement_geojson/:siret' => '/api/v1/near_etablissement_geojson#show'
+    get 'siret/:siret' => '/api/v1/siret#show', :constraints => { :siret => /.*/ }
+    get 'siren/:siren' => '/api/v1/siren#show', :constraints => { :siren => /.*/ }
+    get 'siren_like/:siren' => '/api/v1/siren_like#show', :constraints => { :siren => /.*/ }
+    get 'siret_like/:siret' => '/api/v1/siret_like#show', :constraints => { :siret => /.*/ }
+    get 'nom_adresse/:text' => '/api/v1/nom_adresse#show', :constraints => { :text => /.*/ }
+    get 'full_text/:text' => '/api/v1/full_text#show', :constraints => { :text => /.*/ }
+    get 'suggest/:suggest_query' => '/api/v1/suggest#show', :constraints => { :suggest_query => /.*/ }
+    get 'near_etablissement/:siret' => '/api/v1/near_etablissement#show', :constraints => { :siret => /.*/ }
+    get 'near_etablissement_geojson/:siret' => '/api/v1/near_etablissement_geojson#show', :constraints => { :siret => /.*/ }
     get 'near_point/' => '/api/v1/near_point#show'
-    get 'rna/:rna' => '/api/v1/numero_rna#show'
+    get 'rna/:rna' => '/api/v1/numero_rna#show', :constraints => { :rna => /.*/ }
   end
 
   concern :v2_routes do
